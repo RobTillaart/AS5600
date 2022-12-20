e1184634e4fb74d9be2505ed1345e7e9e23817a4
rob tillaart <rob.tillaart@gmail.com>
2022-12-20 20:08:07 +0100

    add RPM example

---
+++ examples/AS5600_angular_speed_RPM/AS5600_angular_speed_RPM.ino
@@ -0,0 +1,48 @@
+//
+//    FILE: AS5600_angular_speed_RPM.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: demo
+//    DATE: 2022-06-02
+
+
+#include "AS5600.h"
+#include "Wire.h"
+
+AS5600 as5600;   //  use default Wire
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("AS5600_LIB_VERSION: ");
+  Serial.println(AS5600_LIB_VERSION);
+
+  Wire.begin();
+  //  as5600.begin(14,15);
+  as5600.begin(4);  //  set direction pin.
+  as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
+
+  Serial.println(as5600.getAddress());
+
+  // as5600.setAddress(0x40);  // AS5600L only
+
+  int b = as5600.isConnected();
+  Serial.print("Connect: ");
+  Serial.println(b);
+
+  delay(1000);
+}
+
+
+void loop()
+{
+  //  Serial.print("\ta = ");
+  //  Serial.print(as5600.readAngle());
+  Serial.print("\tω = ");
+  Serial.println(as5600.getAngularSpeed(AS5600_MODE_RPM));
+  delay(100);
+}
+
+
+// -- END OF FILE --
