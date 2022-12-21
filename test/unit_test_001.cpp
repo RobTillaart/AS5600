73efb53eb09c6cd745ef1bd7ec56b5c665cfad04
rob tillaart <rob.tillaart@gmail.com>
2022-12-21 20:45:33 +0100

    minor edits

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -54,9 +54,11 @@ unittest(test_constants)
 
   assertEqual(0, AS5600_MODE_DEGREES);
   assertEqual(1, AS5600_MODE_RADIANS);
+  assertEqual(2, AS5600_MODE_RPM);
 
-  assertEqualFloat(360.0/4096, AS5600_RAW_TO_DEGREES, 0.0001);
+  assertEqualFloat(360.0/4096,    AS5600_RAW_TO_DEGREES, 0.0001);
   assertEqualFloat((PI*2.0)/4096, AS5600_RAW_TO_RADIANS, 0.0001);
+  assertEqualFloat(60/4096,       AS5600_RAW_TO_RPM,     0.0001);
 
   assertEqual(0, AS5600_OUTMODE_ANALOG_100);
   assertEqual(1, AS5600_OUTMODE_ANALOG_90);
