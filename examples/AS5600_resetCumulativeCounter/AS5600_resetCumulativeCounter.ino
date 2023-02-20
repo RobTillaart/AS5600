2745a50955b8e7dc88fa29231c201b06e63960ed
rob tillaart <rob.tillaart@gmail.com>
2023-02-20 16:33:49 +0100

    add example

--- examples/AS5600_resetCumulativeCounter/AS5600_resetCumulativeCounter.ino
+++ examples/AS5600_resetCumulativeCounter/AS5600_resetCumulativeCounter.ino
@@ -1,3 +1,13 @@
+//
+//    FILE: AS5600_resetCumulativeCounter.ino
+//  AUTHOR: Daniel-Frenkel, (slightly by Rob Tillaart)
+// PURPOSE: demo - see issue #30
+//    DATE: 2023-0
+
+
+daniel-frenkel
+
+
 #include "AS5600.h"
 #include "Wire.h"
 
@@ -6,12 +16,12 @@ AS5600L as5600;   //  use default Wire
 void setup()
 {
   Serial.begin(115200);
-  as5600.begin(14, 15); //ESP32
-  as5600.setAddress(0x40);
+  as5600.begin(14, 15);      //  ESP32
+  as5600.setAddress(0x40);   //  AS5600L has address
 
-  as5600.setDirection(AS5600_CLOCK_WISE);  // default, just be explicit.
+  as5600.setDirection(AS5600_CLOCK_WISE);  //  default, just be explicit.
   delay(1000);
-  as5600.resetCumulativePosition(777); //  NEW CODE to set the initial value to 777
+  as5600.resetCumulativePosition(777);
 }
 
 
@@ -29,3 +39,7 @@ void loop()
     Serial.println(as5600.getCumulativePosition());
   }
 }
+
+
+//  -- END OF FILE --
+
